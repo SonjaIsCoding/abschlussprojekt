71b809b4527323a9d6949816ac5ccf2c947c9c7d
Sonja Dahlke <sonja-dahlke@web.de>
2024-04-17 11:23:42 -0700

    add navbar and treeview

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,30 @@
+import TreeView from "./TreeView";
+import { FaRegFolder } from "react-icons/fa6";
+import { LuFolderTree } from "react-icons/lu";
+import { FaRegFolderOpen } from "react-icons/fa6";
+import { FaFolderPlus } from "react-icons/fa";
+import { FaFolderMinus } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import { FaTrashRestore } from "react-icons/fa";
+import { FiSidebar } from "react-icons/fi";
+import { RiAccountCircleLine } from "react-icons/ri";
+import { TbFolderPlus } from "react-icons/tb";
+import { VscNewFile } from "react-icons/vsc";
+
 export function Navbar() {
   return (
-    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-      <i>E</i>
+    <div className="fixed z-20 top-0 left-0 h-screen w-50 m-0 flex flex-col bg-[#537791] shadow-2xl">
+      <div className="flex justify-end  bg-[#c1c0b9]">
+        <button>
+          <TbFolderPlus className="text-[#f7f6e7] text-2xl" />
+        </button>
+        <button className="text-[#f7f6e7] text-2xl">
+          <VscNewFile />
+        </button>
+      </div>
+      <div className="text-[#f7f6e7] p-3 text-xl">
+        <TreeView />
+      </div>
     </div>
   );
 }
-
-const NavBarIcon = ({ icon }) => <div className="sidebar-icon">{icon}</div>;
---
+++ src/components/TreeView.jsx
@@ -0,0 +1,130 @@
+import TreeView, { flattenTree } from "react-accessible-treeview";
+import { DiCss3, DiJavascript, DiNpm } from "react-icons/di";
+import { FaList, FaRegFolder, FaRegFolderOpen } from "react-icons/fa";
+import { LuFolderTree } from "react-icons/lu";
+import { FaFolderPlus } from "react-icons/fa";
+import { FaFolderMinus } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import { FaTrashRestore } from "react-icons/fa";
+import { FiSidebar } from "react-icons/fi";
+import { RiAccountCircleLine } from "react-icons/ri";
+
+const folder = {
+  name: "",
+  children: [
+    {
+      name: "Buch",
+      children: [
+        {
+          name: "Kapitel 1",
+          children: [{ name: "Szene 1.txt" }, { name: "Szene 2.txt" }],
+        },
+        {
+          name: "Kapitel 2",
+          children: [
+            { name: "Szene 1.txt" },
+            { name: "Szene 2.txt" },
+            { name: "Szene 3.txt" },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Ideen",
+      children: [
+        { name: "Braindump 1.txt" },
+        { name: "Notizen.txt" },
+        { name: "Noch mehr Ideen.txt" },
+        { name: "AAAaaaahhhhh.txt" },
+      ],
+    },
+    {
+      name: "Charaktere",
+      children: [
+        { name: "Hauptcharakter.txt" },
+        { name: "Nebencharakter.txt" },
+        { name: "Max/Bob/Magnus.txt" },
+      ],
+    },
+    // {
+    //   name: "src",
+    //   children: [{ name: "index.js" }, { name: "styles.css" }],
+    // },
+    // {
+    //   name: "node_modules",
+    //   children: [
+    //     {
+    //       name: "react-accessible-treeview",
+    //       children: [{ name: "index.js" }],
+    //     },
+    //     { name: "react", children: [{ name: "index.js" }] },
+    //   ],
+    // },
+    // {
+    //   name: ".npmignore",
+    // },
+    // {
+    //   name: "package.json",
+    // },
+    // {
+    //   name: "webpack.config.js",
+    // },
+  ],
+};
+
+const data = flattenTree(folder);
+
+function DirectoryTreeView() {
+  return (
+    <div>
+      <div className="directory">
+        <TreeView
+          data={data}
+          aria-label="directory tree"
+          nodeRenderer={({
+            element,
+            isBranch,
+            isExpanded,
+            getNodeProps,
+            level,
+          }) => (
+            <div {...getNodeProps()} style={{ paddingLeft: 20 * (level - 1) }}>
+              {isBranch ? (
+                <FolderIcon isOpen={isExpanded} />
+              ) : (
+                <FileIcon filename={element.name} />
+              )}
+
+              {element.name}
+            </div>
+          )}
+        />
+      </div>
+    </div>
+  );
+}
+
+const FolderIcon = ({ isOpen }) =>
+  isOpen ? (
+    <FaRegFolderOpen color="e8a87c" className="icon" />
+  ) : (
+    <FaRegFolder color="e8a87c" className="icon" />
+  );
+
+const FileIcon = ({ filename }) => {
+  const extension = filename.slice(filename.lastIndexOf(".") + 1);
+  switch (extension) {
+    case "js":
+      return <DiJavascript color="yellow" className="icon" />;
+    case "css":
+      return <DiCss3 color="turquoise" className="icon" />;
+    case "json":
+      return <FaList color="yellow" className="icon" />;
+    case "npmignore":
+      return <DiNpm color="red" className="icon" />;
+    default:
+      return null;
+  }
+};
+
+export default DirectoryTreeView;
